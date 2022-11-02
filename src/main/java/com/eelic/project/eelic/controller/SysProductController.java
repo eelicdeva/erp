59399f5f328b1dc49545f16dd27658fe2d9f31b4
Deva <deva@eelic.com>
2022-11-02 15:51:46 +0700

    seperate version

---
+++ src/main/java/com/eelic/project/eelic/controller/SysProductController.java
@@ -0,0 +1,102 @@
+package com.eelic.project.eelic.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.framework.aspectj.lang.annotation.Log;
+import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
+import com.eelic.project.eelic.domain.SysProduct;
+import com.eelic.project.eelic.service.ISysProductService;
+import com.ruoyi.framework.web.controller.BaseController;
+import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+
+/**
+ * 产品Controller
+ * 
+ * @author eelic
+ * @date 2022-11-02
+ */
+@RestController
+@RequestMapping("/eelic/product")
+public class SysProductController extends BaseController
+{
+    @Autowired
+    private ISysProductService sysProductService;
+
+    /**
+     * 查询产品列表
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:list')")
+    @GetMapping("/list")
+    public AjaxResult list(SysProduct sysProduct)
+    {
+        List<SysProduct> list = sysProductService.selectSysProductList(sysProduct);
+        return AjaxResult.success(list);
+    }
+
+    /**
+     * 导出产品列表
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:export')")
+    @Log(title = "产品", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysProduct sysProduct)
+    {
+        List<SysProduct> list = sysProductService.selectSysProductList(sysProduct);
+        ExcelUtil<SysProduct> util = new ExcelUtil<SysProduct>(SysProduct.class);
+        util.exportExcel(response, list, "产品数据");
+    }
+
+    /**
+     * 获取产品详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:query')")
+    @GetMapping(value = "/{productId}")
+    public AjaxResult getInfo(@PathVariable("productId") Long productId)
+    {
+        return AjaxResult.success(sysProductService.selectSysProductByProductId(productId));
+    }
+
+    /**
+     * 新增产品
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:add')")
+    @Log(title = "产品", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody SysProduct sysProduct)
+    {
+        return toAjax(sysProductService.insertSysProduct(sysProduct));
+    }
+
+    /**
+     * 修改产品
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:edit')")
+    @Log(title = "产品", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody SysProduct sysProduct)
+    {
+        return toAjax(sysProductService.updateSysProduct(sysProduct));
+    }
+
+    /**
+     * 删除产品
+     */
+    @PreAuthorize("@ss.hasPermi('eelic:product:remove')")
+    @Log(title = "产品", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{productIds}")
+    public AjaxResult remove(@PathVariable Long[] productIds)
+    {
+        return toAjax(sysProductService.deleteSysProductByProductIds(productIds));
+    }
+}
