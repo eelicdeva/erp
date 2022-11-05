0871b9bc9862ed5d6cc185bd0e3246ad967aa7ee
Gogs <eason@eelic.com>
2022-11-05 07:58:50 +0700

    sql update

--- sql/genchart.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `erp`.`gen_table` 
-ADD COLUMN `chart_type` CHAR(1) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT '0' COMMENT 'chart type' AFTER `remark`,
-ADD COLUMN `xAxis_chart` VARCHAR(200) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL COMMENT 'xAxis chart data' AFTER `chart_type`,
-ADD COLUMN `yAxis_chart` VARCHAR(200) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL COMMENT 'yAxis chart data' AFTER `xAxis_chart`;
