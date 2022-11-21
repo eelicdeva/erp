39e0dfa619bb61a55383322d4682a19e2f37aa72
Deva <deva@eelic.com>
2022-11-21 11:30:53 +0700

    move Folder

---
+++ sql/moveFolder.sql
@@ -0,0 +1,4 @@
+UPDATE `erp`.`sys_menu` SET `component` = 'erp/hr/book/category/index' WHERE (`menu_id` = '1086');
+UPDATE `erp`.`sys_menu` SET `component` = 'erp/hr/book/location/index' WHERE (`menu_id` = '1092');
+UPDATE `erp`.`sys_menu` SET `component` = 'erp/hr/book/loan/index' WHERE (`menu_id` = '1079');
+UPDATE `erp`.`sys_menu` SET `component` = 'erp/hr/book/statistics/index' WHERE (`menu_id` = '1085');
