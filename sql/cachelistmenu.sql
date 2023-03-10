159b2d09aabb2e6a44e05f773b03d5f38f9d0a7a
Deva <deva@eelic.com>
2023-03-10 14:17:43 +0700

    cache list sql

---
+++ sql/cachelistmenu.sql
@@ -0,0 +1,4 @@
+insert into sys_menu (menu_name, parent_id, order_num, path, component, query, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark, menu_name_en, menu_name_id, menu_name_tw)
+values('缓存列表', '2', '6', 'cacheList', 'monitor/cache/list', NULL, '1', '0', 'C', '0', '0', 'monitor:cache:list', 'redis-list', 'admin', '2023-03-09 09:53:26', 'admin', '2023-03-09 09:54:04', '', 'Cache List', 'List Cache', NULL);
+
+
